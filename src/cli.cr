3baef52c922c5a6690fbd1f3f4cdbb542649c805
Will Lewis <will@wflewis.com>
2019-09-04 02:06:26 -0400

    Improve usage

--- src/cli.cr
+++ src/cli.cr
@@ -2,7 +2,7 @@ require "option_parser"
 require "./durdiff"
 
 parser = OptionParser.new do |parser|
-  parser.banner = "usage: durdiff"
+  parser.banner = "usage: durdiff [args] DIR1 DIR2"
 
   parser.on("-v", "--version", "display the version") { puts Durdiff::VERSION; exit 0 }
   parser.on("-h", "--help", "show this help") { puts parser; exit 0 }
