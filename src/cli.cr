a926cfbd523a082103c89d4bec5838663fc04104
Will Lewis <will@wflewis.com>
2019-09-04 02:03:19 -0400

    Use set union and clean up the set pipelines

--- src/cli.cr
+++ src/cli.cr
@@ -21,31 +21,25 @@ left, right = Durdiff.get_lists
 
 left_set = Set.new(left.entries)
 right_set = Set.new(right.entries)
-joint = Set.new(left.entries + right.entries)
+joint = left_set | right_set
 
 sorted_left =
   joint
     .to_a
     .sort
-    .map do |x|
-      if left_set.includes?(x)
-        x
-      else
-        ""
-      end
-    end
+    .map { |x| Set{x} }
+    .map { |x| x & left_set }
+    .map(&.first?)
+    .map { |x| x || "" }
 
 sorted_right =
   joint
     .to_a
     .sort
-    .map do |x|
-      if right_set.includes?(x)
-        x
-      else
-        ""
-      end
-    end
+    .map { |x| Set{x} }
+    .map { |x| x & right_set }
+    .map(&.first?)
+    .map { |x| x || "" }
 
 box = Durdiff::Box::QuadBox.new(
   left.path,
